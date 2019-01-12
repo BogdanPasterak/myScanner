091d66e1e04f444955316581ae05cb5b7ad4155a
BogdanPasterak <bogdanpasterak@gmail.com>
2019-01-12 22:56:52 +0000

    feat: test Junit

---
+++ src/myScannerTest/MS3JUnitTest.java
@@ -0,0 +1,56 @@
+package myScannerTest;
+
+import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import myScanner.MS3;
+
+public class MS3JUnitTest {
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+
+    @Test
+    public void testCase1() {
+        final String testString = "yes";
+        final String onScreen = "info : ";
+        provideInput(testString);
+
+        String output = MS3.getString("info", MS3.ALLOW_FILL, "yes", "no");
+        assertEquals(testString, output);
+        assertEquals(onScreen, getOutput());
+    }
+
+}
--- src/myScannerTest/MS3Test.java
+++ src/myScannerTest/MS3Test.java
@@ -2,47 +2,68 @@ package myScannerTest;
 
 import java.awt.AWTException;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import myScanner.MS3;
 
 public class MS3Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		InputStream stdin = System.in;
 		boolean allTest = true;
 
 		String testS;
 
-		// Type Enter getSrting( all params)
+		// Type yes and Enter getSrting( all params)
 		// set answer for test
 		System.setIn(new ByteArrayInputStream("yes\r\n".getBytes()));
 		// test
 		testS = MS3.getString("info", MS3.ALLOW_FILL, "yes", "no");
 		// result
-		if (allTest = allTest && (testS.equals("yes") || testS.equals("no")))
+		if (allTest = oneOf(testS, allTest, "yes", "no"))
 			System.out.println("Pass");
 		else
 			System.out.println("\nFail   output = " + testS);
 
-/*
-		// Type no getSrting( all params)
-		System.setIn(new ByteArrayInputStream("\r\n".getBytes()));
+		
+		// Type no and Enter getSrting( all params)
+		System.setIn(new ByteArrayInputStream("no\r\n".getBytes()));
 		testS = MS3.getString("info", MS3.ALLOW_FILL, "yes", "no");
-		if (allTest = allTest && (testS == "yes" || testS == "no"))
+		if (allTest = oneOf(testS, allTest, "yes", "no"))
 			System.out.println("Pass");
 		else
 			System.out.println("\nFail   output = " + testS);
-*/
+
+		// Type no and Enter getSrting( all params)
+		System.setIn(new ByteArrayInputStream("no\r\n".getBytes()));
+		testS = MS3.getString("info", MS3.ALLOW_FILL, "yes", "no");
+		if (allTest = oneOf(testS, allTest, "yes", "no"))
+			System.out.println("Pass");
+		else
+			System.out.println("\nFail   output = " + testS);
+
+
 		// back original setting
 		System.setIn(stdin);
 		System.out.println("All test = " + allTest);
 
 	}
 
-	public static void setTest() {
-
+	
+	public static boolean oneOf(String toTest, boolean before, String... strings ) {
+		boolean answer;
+		
+		if (!before)
+			return false;
+		
+		for (String s : strings) {
+			if (toTest.equals(s))
+				return true;
+		}
+		
+		return false;
 	}
 
 }
