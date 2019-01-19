5709a48107693ca0a839dc3570e772873ab62e37
Bogdan Pasterak <bogdanpasterak@gmail.com>
2019-01-19 16:52:51 +0000

    feat: test for getChar( all param)

--- src/myScannerTest/MS3TestGetChar.java
+++ src/myScannerTest/MS3TestGetChar.java
@@ -17,8 +17,8 @@ public class MS3TestGetChar {
 		char test;
 		String[] answers = {
 			// getSrting( all params)
-				// t01 , t02   , t003  , t004                       ,
-				"a\r\n", "\r\n"
+				// t01 , t02   , t03      , t04    , t05
+				"a\r\n", "\r\n", "ala\r\n", "z\r\n", "\r\n", "z\r\n", "a\r\n"
 				
 				
 		};
@@ -31,7 +31,7 @@ public class MS3TestGetChar {
 		System.setIn(new ByteArrayInputStream(testS.getBytes()));
 
 		System.out.println("Test t01 one entry");
-		// t001 Type yes and Enter getSrting( all params)
+		// t01 Type a and Enter getSrting( all params)
 		test = MS3.getChar("info", MS3.ALLOW_FILL, MS3.CHARS.ALL_DIGITS, MS3.CHARS.WITH_WHITE_SIGNS, "abc12345");
 		// result
 		if (allTest = oneOf(test, allTest, "a"))
@@ -39,14 +39,43 @@ public class MS3TestGetChar {
 		else
 			System.out.println("\nFail   output = " + testS);
 
-		System.out.println("Test t01 one entry");
-		// t001 Type yes and Enter getSrting( all params)
+		System.out.println("Test t02 one entry");
+		// t02 Type Enter getSrting( all params)
 		test = MS3.getChar("info", MS3.ALLOW_FILL, MS3.CHARS.ALL_DIGITS, MS3.CHARS.WITH_WHITE_SIGNS, "abc12345");
 		// result
 		if (allTest = oneOf(test, allTest, "0123456789 _abc"))
 			System.out.println(test + "\nPass");
 		else
 			System.out.println("\nFail   output = " + testS);
+
+		System.out.println("Test t03 one entry");
+		// t03 Type ala and Enter change to a getSrting( all params)
+		test = MS3.getChar("info", MS3.ALLOW_FILL, MS3.CHARS.ALL_DIGITS, MS3.CHARS.WITH_WHITE_SIGNS, "abc12345");
+		// result
+		if (allTest = oneOf(test, allTest, "a"))
+			System.out.println("ala" + "\nPass");
+		else
+			System.out.println("\nFail   output = " + testS);
+
+		System.out.println("Test t04 one entry");
+		// t04 Type z and Enter getSrting( all params)
+		test = MS3.getChar("info", MS3.ALLOW_FILL, MS3.CHARS.ALL_DIGITS, MS3.CHARS.WITH_WHITE_SIGNS, "abc12345");
+		// result
+		if (allTest = oneOf(test, allTest, "z"))
+			System.out.println(test + "\nPass");
+		else
+			System.out.println("\nFail   output = " + testS);
+
+		System.out.println("Test t05 three entry");
+		// t05 Type Enter retype z and retype a getSrting( all params)
+		test = MS3.getChar("", MS3.DONT_FILL, MS3.CHARS.ALL_DIGITS, MS3.CHARS.WITH_WHITE_SIGNS, "abc12345");
+		// result
+		if (allTest = oneOf(test, allTest, "a"))
+			System.out.println(test + "\nPass");
+		else
+			System.out.println("\nFail   output = " + testS);
+		
+		
 
 		
 		
